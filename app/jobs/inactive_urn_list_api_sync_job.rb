b7586f0f658b4040a4f27a15b7fbafd8c47da23a
Reem Ibrahim <reem.ibrahim@crowncommercial.gov.uk>
2026-05-29 17:05:53 +0100

    updated sync jobs and spec

--- app/jobs/inactive_urn_list_api_sync_job.rb
+++ app/jobs/inactive_urn_list_api_sync_job.rb
@@ -1,6 +1,6 @@
 class InactiveUrnListApiSyncJob < ApplicationJob
   def perform
-    rows = UrnLists::ApiClient.new.fetch_inactive_customers
+    rows = UrnLists::ApiClient.new.fetch_inactive_rows
 
     UrnLists::ImportInactiveCustomers.new(rows: rows).call
   end
